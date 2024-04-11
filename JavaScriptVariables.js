b5992ba53ba6be5d4ea3d24b98592eea95b6ba90
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-11 20:19:46 +0600

    explainig let

--- JavaScriptVariables.js
+++ JavaScriptVariables.js
@@ -5,13 +5,19 @@
 
 1. variables declared with var r function-scoped or globally scoped.
 2. they can be reassigned and redeclared within its scope. (weakness)*/
-var x = 5
-var y = 10
-x = 20 // reassignment
-var x = 30 // redeclaration
-console.log(x)
 
+var x = 5;
+var y = 10;
+x = 20; // reassignment
+var x = 30; // redeclaration
+console.log(x);
 
 /*------------let-----------
 1. Variables declared with let r block-scoped.
 2. they can be reassigned within its block scope, but cannot be redeclared in the same scope.*/
+
+let a = 5;
+a = 10; // reassignment
+// let a = 15; // error: identifier 'a' has already been declared, so cannot be redeclared
+
+console.log(a);
