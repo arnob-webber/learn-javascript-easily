ad1237dbff5521b1908cd24b191716744b63bdc6
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-11 19:28:02 +0600

    assign values

---
+++ javaScript operators/AssignmentOperators.js
@@ -0,0 +1,7 @@
+// these are used to assign values to variables
+
+let a = 5;
+a += 3; // equivalent to: a = a + 3;
+a -= 2; // equivalent to: a = a - 2;
+a *= 4; // equivalent to: a = a * 4;
+a /= 2; // equivalent to: a = a / 2;
