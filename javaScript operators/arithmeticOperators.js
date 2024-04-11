651a8812c13412ed9cee8d93d65ab3a0662000fe
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-11 19:22:39 +0600

    arithmetic operators

---
+++ javaScript operators/arithmeticOperators.js
@@ -0,0 +1,7 @@
+let x = 10; 
+let y = 5;
+let addition = x + y;
+let subtraction = x - y;
+let multiplication = x * y;
+let division = x / y;
+let remainder = x % y; // Modulus (remainder of division)
