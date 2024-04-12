1ddfc486d65bd28371e42d3f6f0ff12ea883e4a8
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-12 21:51:04 +0600

    js prompt

---
+++ JavaScript-prompt.js
@@ -0,0 +1,6 @@
+let cookie = prompt("agree to accept cookie");
+
+if (cookie.toLowerCase() === "yes") {
+  // toLowerCase() is used to convert the user input to lowercase before comparing it with "yes".
+  console.log("u're in a trap, my boy");
+}
