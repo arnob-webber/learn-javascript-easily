3c366b8741fa6ff24b577a631eeba3b68e37f5d9
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-12 21:36:31 +0600

    js arrary

---
+++ JavaScript_For-Loop.js
@@ -0,0 +1,3 @@
+for (Name = 0; Name >= 5; Name++) {
+  console.log("i m arnob");
+}
---
+++ Js-array.js
@@ -0,0 +1,6 @@
+let myGf = ["nabila", "ishu", "eru", "nowrin"];
+
+console.log("i love" + " " + myGf[0] + ", and" + " " + myGf[2] + " " + "also");
+
+// " " for extra space
+//  for concatenate strings +
